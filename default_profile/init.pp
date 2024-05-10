9bae8928c664b43c6a242a591b768ebc4987175e
daviddmunozzz <118215871+daviddmunozzz@users.noreply.github.com>
2024-05-10 02:39:35 +0200

    Update init.pp

---
+++ default_profile/init.pp
@@ -0,0 +1,24 @@
+class default_profile {
+    # Crear directorios
+    file { ['/tmp/php', '/etc/config', '/tmp/photos']:
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    # Instalar paquetes
+    package { ['htop', 'gimp', 'vim', 'python3']:
+        ensure => installed,
+    }
+
+    # Crear archivo con cadena indicando acciones realizadas
+    file { '/etc/config/puppet_actions2.log':
+        ensure  => present,
+        content => "[DEF]Se crearon los directorios /tmp/php, /etc/config y /tmp/photos. Se instalaron los paquetes htop, gimp y vim",
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+}

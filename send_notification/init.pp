78f66cb9ac72a2df40ca55e8ae4e2f2954af1351
daviddmunozzz <118215871+daviddmunozzz@users.noreply.github.com>
2024-05-10 02:40:09 +0200

    Update init.pp

---
+++ send_notification/init.pp
@@ -0,0 +1,16 @@
+class send_notification{
+    file { '/tmp/notify.py':
+    ensure  => present,
+    content => file('/etc/notify/send_notification.py'),
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+
+    }
+
+    exec { 'send_notification':
+    command => '/usr/bin/python3 /tmp/notify.py /etc/config/puppet_actions2.log',
+    logoutput => true,
+
+    }
+}

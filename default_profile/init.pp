d124456a9145f1c83d56c82d54781d00c110d96e
daviddmunozzz <118215871+daviddmunozzz@users.noreply.github.com>
2024-05-10 02:44:57 +0200

    Update init.pp

--- default_profile/init.pp
+++ default_profile/init.pp
@@ -13,7 +13,7 @@ class default_profile {
     }
 
     # Crear archivo con cadena indicando acciones realizadas
-    file { '/etc/config/puppet_actions2.log':
+    file { '/etc/config/puppet_actions.log':
         ensure  => present,
         content => "[DEF]Se crearon los directorios /tmp/php, /etc/config y /tmp/photos. Se instalaron los paquetes htop, gimp y vim",
         mode    => '0644',
